cf4275a2254c8852b737b76341eba4aced9b07a0
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-06-26 22:34:08 +0200

    Build Cython moduels

--- setup.py
+++ setup.py
@@ -1,4 +1,30 @@
 from setuptools import setup, find_packages
+from setuptools.extension import Extension
+from Cython.Build import cythonize
+
+import os
+import glob
+import numpy as np
+
+base_path = ["arbitrary_woodland"]
+
+source_files = [
+    *glob.glob(os.path.join(*base_path, "*.pyx"))
+]
+
+include_dirs = [
+    os.path.join(*base_path),
+    np.get_include()
+]
+
+extensions = [
+    Extension(
+        name="arbitrary_woodland._tree",
+        sources=source_files,
+        language="c",
+        include_dirs=include_dirs
+    )
+]
 
 
 def _long_description():
@@ -11,4 +37,5 @@ setup(
     version="0.0.1",
     long_description=_long_description(),
     packages=find_packages(),
+    ext_modules=cythonize(extensions)
 )
