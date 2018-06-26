c36ab3e9001c2ea0bb68b3ac6a596ec184fc2abd
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-06-26 12:17:25 +0200

    Make default max_depth to None

--- arbitrary_woodland/tree.py
+++ arbitrary_woodland/tree.py
@@ -3,7 +3,7 @@ from arbitrary_woodland.gini import gini_impurity
 
 
 class DecisionTree:
-    def __init__(self, max_depth=100, min_size=4, num_features="auto"):
+    def __init__(self, max_depth=None, min_size=4, num_features="auto"):
         self.max_depth = max_depth
         self.min_size = min_size
         self.num_features = num_features
@@ -76,7 +76,7 @@ class DecisionTree:
 
             return
 
-        if depth >= self.max_depth:
+        if self.max_depth is not None and depth >= self.max_depth:
             node["left"] = self._end_node(left_y)
             node["right"] = self._end_node(right_y)
 
