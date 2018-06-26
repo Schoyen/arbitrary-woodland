08df5cd0ed91d689674698e6c48a2d87bb2f0ba3
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-06-26 22:02:04 +0200

    Use numba for the Gini impurity

--- arbitrary_woodland/gini.py
+++ arbitrary_woodland/gini.py
@@ -1,15 +1,19 @@
 import numpy as np
+import numba
 
 
+@numba.njit(cache=True)
 def gini_impurity(groups: np.ndarray, classes: np.ndarray) -> float:
     gini = 0
 
-    for group in groups:
+    for i in range(len(groups)):
+        group = groups[i]
         if len(group) == 0:
             continue
 
         score = 0
-        for _class in classes:
+        for j in range(len(classes)):
+            _class = classes[j]
             ratio_class = np.sum(group == _class) / len(group)
             score += ratio_class ** 2
 
