adf9bc701b1910cdf4982746113ad248b673a83b
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-06-26 23:00:43 +0200

    Use Cython version of get_split

--- arbitrary_woodland/tree.py
+++ arbitrary_woodland/tree.py
@@ -1,5 +1,6 @@
 import numpy as np
 from arbitrary_woodland.gini import gini_impurity
+from arbitrary_woodland._tree import get_split
 
 
 class DecisionTree:
@@ -12,7 +13,7 @@ class DecisionTree:
         if self.num_features == "auto":
             self.num_features = round(float(np.sqrt(len(X[0]))))
 
-        self.root = self._get_split(X, y)
+        self.root = get_split(X, y, self.num_features)
         self._split(self.root, 1)
 
         return self
@@ -38,30 +39,6 @@ class DecisionTree:
             else:
                 return node["right"]
 
-    def _get_split(self, X, y) -> dict:
-        classes = np.unique(y)
-        node = {"index": 1000, "value": 1000, "groups": None}
-
-        score = 1000
-
-        indices = np.random.choice(
-            len(X[0]), size=self.num_features, replace=False
-        )
-
-        for index in indices:
-            for i in range(len(X)):
-                group_indices = X[:, index] < X[i, index]
-                groups = (X[group_indices], X[~group_indices])
-                gini = gini_impurity(groups, classes)
-
-                if gini < score:
-                    node["index"] = index
-                    node["value"] = X[i, index]
-                    node["groups"] = groups
-                    node["groups_y"] = (y[group_indices], y[~group_indices])
-
-        return node
-
     def _split(self, node: dict, depth: int) -> None:
         left, right = node["groups"]
         left_y, right_y = node["groups_y"]
@@ -85,13 +62,13 @@ class DecisionTree:
         if len(left) <= self.min_size:
             node["left"] = self._end_node(left_y)
         else:
-            node["left"] = self._get_split(left, left_y)
+            node["left"] = get_split(left, left_y, self.num_features)
             self._split(node["left"], depth + 1)
 
         if len(right) <= self.min_size:
             node["right"] = self._end_node(right_y)
         else:
-            node["right"] = self._get_split(right, right_y)
+            node["right"] = get_split(right, right_y, self.num_features)
             self._split(node["right"], depth + 1)
 
     def _end_node(self, y):
