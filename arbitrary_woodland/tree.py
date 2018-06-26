12dfba5797f0a00790b20516d7e5920bc808d8fa
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-06-26 12:09:34 +0200

    Use default values for parameters

--- arbitrary_woodland/tree.py
+++ arbitrary_woodland/tree.py
@@ -3,7 +3,7 @@ from arbitrary_woodland.gini import gini_impurity
 
 
 class DecisionTree:
-    def __init__(self, max_depth: int, min_size: int, num_features: int):
+    def __init__(self, max_depth=100, min_size=4, num_features="auto"):
         self.max_depth = max_depth
         self.min_size = min_size
         self.num_features = num_features
